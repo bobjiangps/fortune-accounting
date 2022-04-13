72307cd014d172ecbf1653e9eb8726172bbefd08
Bo Jiang <bo.jiang@activenetwork.com>
2022-04-13 14:54:34 +0800

    add bottom nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,129 +1,90 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
-// #docregion MyApp
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // #docregion build
+  static const String _title = 'App Title';
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Startup Name Generator MyApp',
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-      home: const Accounting(),
+    return const MaterialApp(
+      title: _title,
+      home: MyStatefulWidget(),
     );
   }
-  // #enddocregion build
 }
-// #enddocregion MyApp
 
-// #docregion RWS-var
-class _AccountingState extends State<Accounting> {
-  final _suggestions = <WordPair>[];
-  final _saved = Set<WordPair>();
-  final _biggerFont = const TextStyle(fontSize: 18);
-  // #enddocregion RWS-var
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
 
-  // #docregion _buildSuggestions
-  Widget _buildSuggestions() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16),
-      itemBuilder: /*1*/ (context, i) {
-        if (i.isOdd) {
-          return const Divider(); /*2*/
-        }
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
 
-        final index = i ~/ 2; /*3*/
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-        }
-        return _buildRow(_suggestions[index]);
-      },
-    );
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      '支出',
+      style: optionStyle,
+    ),
+    Text(
+      '收入',
+      style: optionStyle,
+    ),
+    Text(
+      '图表',
+      style: optionStyle,
+    ),
+    Text(
+      '设置',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
-  // #enddocregion _buildSuggestions
 
-  // #docregion _buildRow
-  Widget _buildRow(WordPair pair) {
-    final bool alreadySaved = _saved.contains(pair);
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
-    );
-  }
-  // #enddocregion _buildRow
-
-  // #docregion RWS-build
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Startup Name Generator override'),
-        actions: <Widget>[
-          IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved),
+        title: const Text('简单记账'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.paid),
+            label: '支出',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.paid_outlined),
+            label: '收入',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.query_stats),
+            label: '图表',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: '设置',
+          ),
         ],
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-
-  // #enddocregion RWS-build
-  // #docregion RWS-var
-  void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (BuildContext context) {
-          final Iterable<ListTile> tiles = _saved.map(
-            (WordPair pair) {
-              return ListTile(
-                title: Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final List<Widget> divided = ListTile.divideTiles(
-            context: context,
-            tiles: tiles,
-          ).toList();
-
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text('Saved Suggestions'),
-            ),
-            body: ListView(children: divided),
-          );
-        },
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
       ),
     );
   }
-}
-// #enddocregion RWS-var
-
-class Accounting extends StatefulWidget {
-  const Accounting({Key? key}) : super(key: key);
-
-  @override
-  _AccountingState createState() => _AccountingState();
 }
